4ecdc24209387b4bbc950379f2b2b6c74f40c36f
David fox <davidfox967@yahoo.com>
2022-07-30 12:42:36 -0600

    created app component

---
+++ src/components/app/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+
+
+
+class app extends React.Component {
+
+  
+  render() {
+    return (
+      <div>
+        <h1>Ja<span className='highlight'>mmm</span>ing</h1>
+        <div className='App'>
+          <SearchBar/>
+          <div className='App-playlist'>
+            <SearchResults/>
+            <Playlist/>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+
+
+
+
+}
+
+
+
