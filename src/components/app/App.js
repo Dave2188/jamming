84772b70e2676d5c77873769c3846a168dc4b2bd
David fox <davidfox967@yahoo.com>
2022-07-30 19:07:03 -0600

    completed component structures and has rendered

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,32 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './App.css';
-
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+import SearchBar from "../SearchBar/SearchBar";
+import SearchResults from "../SearchResults/SearchResults";
+import Playlist from "../Playlist/Playlist";
 
 export default class App extends React.Component {
+	constructor(props) {
+		super(props);
+	}
 
-
-  render() {
-    return (
-      <div>
-        <h1>Ja<span className='highlight'>mmm</span>ing</h1>
-        <div className='App'>
-          <SearchBar/>
-          <div className='App-playlist'>
-            <SearchResults/>
-            <Playlist/>
-          </div>
-        </div>
-      </div>
-    )
-  }
-
-
-
-
-
+	render() {
+		return (
+			<div>
+				<h1>
+					Ja<span className="highlight">mmm</span>ing
+				</h1>
+				<div className="App">
+					<SearchBar />
+					<div className="App-playlist">
+						<SearchResults />
+						<Playlist />
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
-
-
-
