5511eac6020452448fcd3564064241ffe48f92cf
David fox <davidfox967@yahoo.com>
2022-08-05 20:17:43 -0600

    created playlist name state change

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -4,11 +4,15 @@ import "./App.css";
 import SearchBar from "../SearchBar/SearchBar";
 import SearchResults from "../SearchResults/SearchResults";
 import Playlist from "../Playlist/Playlist";
+import TrackList from "../TrackList/TrackList";
+import { toHaveDisplayValue } from "@testing-library/jest-dom/dist/matchers";
 
 export default class App extends React.Component {
 	constructor(props) {
 		super(props);
 		this.addTrack = this.addTrack.bind(this);
+		this.removeTrack = this.removeTrack.bind(this);
+		this.updatePlayListName = this.updatePlayListName.bind(this);
 		this.state = {
 			searchResults: [
 				{ name: "john", artist: "dave", album: "rocket", id: 851 },
@@ -27,13 +31,34 @@ export default class App extends React.Component {
 	}
 
 	addTrack(track) {
-		console.log(track);
-		if (this.state.playListTracks.find((savedTrack) => savedTrack.id === track.id)) {
+		// console.log(track);
+		if (
+			this.state.playListTracks.find((savedTrack) => savedTrack.id === track.id)
+		) {
+			return;
+		} else {
+			this.state.playListTracks.push(track);
+			this.setState(this.state.playListTracks);
+		}
+	}
+
+	removeTrack(track) {
+		if (
+			this.state.playListTracks.find((savedTrack) => savedTrack.id === track.id)
+		) {
+			let trackToRemove = this.state.playListTracks.indexOf(track);
+			this.state.playListTracks.splice(trackToRemove, 1);
+			this.setState(this.state.playListTracks);
+		} else {
 			return;
 		}
 	}
-    //  add the rest of the code to push to playlist
-	removeTrack()
+
+	updatePlayListName(name) {
+		let newPlaylistName = name;
+		this.state.playListName = newPlaylistName;
+		this.setState({ playListName: name });
+	}
 
 	render() {
 		return (
@@ -49,6 +74,8 @@ export default class App extends React.Component {
 							searchResults={this.state.searchResults}
 						/>
 						<Playlist
+							onNameChange={this.updatePlayListName}
+							onRemove={this.removeTrack}
 							playListName={this.state.playListName}
 							playListTracks={this.state.playListTracks}
 						/>
